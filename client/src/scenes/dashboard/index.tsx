6e09370f99d2d2f00dcf94e3bb344475907834bd
Fuzail Sami <samifuzail@gmail.com>
2025-07-15 13:46:06 -0500

    revenue and expenses chart

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -54,6 +54,7 @@ const Dashboard = () => {
   return (
     <Box width="100%" height="100%" display="grid" gap="1.5rem"
     p="1.5rem"
+    
     sx={
         isAboveMediumScreens ?{
         gridTemplateColumns:"repeat(3,minmax(370px,1fr))",
---
+++ client/src/state/types.ts
@@ -0,0 +1,34 @@
+export interface ExpensesByCatergory{
+    salaries:number;
+    supplies:number;
+    services:number;
+}
+
+export interface Month{
+    id:string;
+    month:string;
+    revenue:number;
+    expenses:number;
+    nonOperationalExpenses:number;
+    operationalExpenses:number;
+}
+export interface Day{
+    id:string;
+    date:string;
+    revenue:number;
+    expenses:number;
+
+}
+
+
+export interface GetKpisResponse{
+    id:string;
+    _id:string;
+    __v:number;
+    totalProfit:number;
+    totalRevenue:number;
+    totalExpenses:number;
+    expensesByCategory:ExpensesByCatergory;
+    monthlyData:Array<Month>;
+    dailyData:Array<Day>;
+}
